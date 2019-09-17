63453862f459a045cef2946b4fe2d3ab5eddebbf
Edward Evans <ed.eevans@gmail.com>
2019-09-16 20:56:29 -0500

    Added implementation and bonus material.

--- WeeklyChallengeApiUsage/Program.cs
+++ WeeklyChallengeApiUsage/Program.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace WeeklyChallengeApiUsage
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
-            
+            await Bootstrap();
+        }
+
+        private static async Task Bootstrap()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            var services = new ServiceCollection()
+                .AddLogging(builder =>
+                {
+                    // Add Serilog
+                    builder.AddSerilog();
+                });
+
+            // Register a HTTP Client
+            services.AddHttpClient<SwapiService>();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            // This is the Microsoft Logging interface
+            var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+
+            var service = serviceProvider.GetRequiredService<SwapiService>();
+            while (true)
+            {
+                Console.WriteLine("Enter a person id to download:");
+                var input = Console.ReadLine();
+                try
+                {
+                    var id = Convert.ToInt32(input);
+                    var response = await service.GetPersonById(id);
+                    if (response.Success)
+                    {
+                        logger.LogInformation($"Name: {response.ServicePeople.Name}");
+                        logger.LogInformation($"Gender: {response.ServicePeople.Gender}");
+                    }
+                    else
+                    {
+                        logger.LogInformation(response.ServiceException.Message);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            }
         }
     }
 }
---
+++ WeeklyChallengeApiUsage/SWAPIService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WeeklyChallengeApiUsage
+{
+    public class SwapiService
+    {
+        private readonly HttpClient _client;
+        private Dictionary<int, People> _cache;
+
+        public SwapiService(HttpClient client)
+        {
+            _client = client;
+            _cache = new Dictionary<int, People>();
+        }
+
+        public async Task<ISwapiServiceResponse> GetPersonById(int id)
+        {
+            var jsonSerializer = new JsonSerializer();
+
+            try
+            {
+                if (_cache.ContainsKey(id))
+                    return SwapiServiceResponse.Ok(_cache[id]);
+
+                var request = CreateRequest(id);
+                var response = await _client.SendAsync(request);
+                if (!response.IsSuccessStatusCode)
+                    throw new HttpRequestException($"{response.StatusCode} - {response.ReasonPhrase}");
+
+                await using var responseStream = await response.Content.ReadAsStreamAsync();
+                using var streamReader = new StreamReader(responseStream);
+                using var jsonTextReader = new JsonTextReader(streamReader);
+                var people = jsonSerializer.Deserialize<People>(jsonTextReader);
+                _cache.Add(id, people);
+                return SwapiServiceResponse.Ok(people);
+            }
+            catch (Exception e)
+            {
+                return SwapiServiceResponse.Error(e);
+            }
+        }
+
+        private HttpRequestMessage CreateRequest(int id)
+        {
+            return new HttpRequestMessage(HttpMethod.Get, $"https://www.swapi.co/api/people/{id}/");
+        }
+    }
+
+    public interface ISwapiServiceResponse
+    {
+        bool Success { get; set; }
+        People ServicePeople { get; set; }
+        Exception ServiceException { get; set; }
+    }
+
+    public class SwapiServiceResponse : ISwapiServiceResponse
+    {
+        public static SwapiServiceResponse Ok(People people)
+        {
+            return new SwapiServiceResponse() { Success = true, ServicePeople = people };
+        }
+
+        public static SwapiServiceResponse Error(Exception ex)
+        {
+            return new SwapiServiceResponse() { Success = false, ServiceException = ex };
+        }
+
+        public bool Success { get; set; }
+        public People ServicePeople { get; set; }
+        public Exception ServiceException { get; set; }
+    }
+
+    public class People
+    {
+        [JsonProperty(PropertyName="name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "gender")]
+        public string Gender { get; set; }
+    }
+}
